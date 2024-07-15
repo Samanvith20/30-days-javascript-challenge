ee7c86437688ab8744b9a2f644f0fb412302887b
Samanvith <Samanvith2005@gmail.com>
2024-07-15 21:12:23 +0530

    Added Readme and changed some code

--- JS Challenge/Day2/Operators.js
+++ JS Challenge/Day2/Operators.js
@@ -1,51 +1,102 @@
-// Task 1: Variable Creation
-var a = 10;
-console.log(a); // Output: 10
 
-let b = "chai aur code";
-console.log(b); // Output: chai aur code
+//  Activity1:Arithmetic Operators
 
-// Task 2: Constant Declaration
-const c = true;
-console.log(c); // Output: true
+// Task1:add two numbers
+let a=10
+let b=20
+let c=a+b
+console.log(c);
 
-// Task 3: Data Types
-let d = 10;
-let e = "chai aur coffee";
-let f = true;
-let g = [1, 2, 3, 4, 5];
-let h = { chai: "chai", coffee: "coffee" };
-let i = null;
+// Task2:subtract two numbers
+let Onenumber=10
+let Twonumber=20
+let Subtract=Twonumber-Onenumber
+console.log(Subtract);
 
-console.log(typeof(d)); // Output: number
-console.log(typeof(e)); // Output: string
-console.log(typeof(f)); // Output: boolean
-console.log(typeof(g)); // Output: object
-console.log(typeof(h)); // Output: object
-console.log(typeof(i)); // Output: object (Note: typeof null is "object" due to a historical quirk in JavaScript)
+// Task3:multiply two numbers
+let Firstnumber=10
+let Secondnumber=20
+let Multiply=Firstnumber*Secondnumber
+console.log(Multiply);
 
-// Task 4: Reassign the Variable
-// Declare a variable and assign an initial value
-let myVariable = 10;
-console.log("Initial value:", myVariable); // Output: Initial value: 10
+// Task4:divide two numbers
+let First=10
+let Second=20
+let Divide=Second/First
+console.log(Divide);
 
-// Reassign a new value to the variable
-myVariable = 20;
-console.log("New value:", myVariable); // Output: New value: 20
+//Task5:To find the remainder
+let one=10
+let two=20
+let Remainder=two%one
+console.log(Remainder);
 
-// Task 5: Const
-// Declare a constant and assign an initial value
-const myConstant = 10;
-console.log("Initial value:", myConstant); // Output: Initial value: 10
+// Actvity2:Assignment Operators
 
-// Attempt to reassign a new value to the constant
-try {
-    myConstant = 20; // This will cause an error
-} catch (error) {
-    console.error("Error when reassigning myConstant:", error.message);
-    // Output: Error when reassigning myConstant: Assignment to constant variable.
+//Task6:use "+=" operator to add a number
+let x=10
+x+=20
+console.log(x);
+
+//Task7:use "-=" operator to subtract a number
+let y=20
+y-=10
+console.log(y);
+
+//Activity3:Comparison Operators
+
+//Task8,9,10
+
+let compare1=20
+let compare2=10
+
+let Task8=compare1>compare2
+let Task8Compare=compare1<compare2
+console.log(Task8,Task8Compare);
+
+let Task9=compare1>=compare2
+let Task9Compare=compare1<=compare2
+console.log(Task9,Task9Compare);
+
+let Task10=compare1==compare2
+let Task10Compare=compare1===compare2
+console.log(Task10,Task10Compare);
+
+//Activity4:Logical Operators
+
+//Task11:uses && and combine two conditions  
+let logicalOne = 10;
+let logicalTwo= 20;
+
+// Combine two conditions using && and log the result to the console
+if (logicalOne > 5 && logicalTwo > 15) {
+  console.log("Both conditions are true");
+} else {
+  console.log("At least one condition is false");
 }
 
-// Error occurs because we cannot reassign a new value to a constant variable.
+//Task12:uses || and combine two conditions
+let logicalThree = 10;
+let logicalFour = 20;
+
+if(logicalThree > 15 || logicalFour > 15){
+    console.log("At least one condition is true");
+}
+
+//Task13:uses ! to negate a condition
+// Define a variable
+let isRaining = true;
+
+// Use the ! operator to negate the condition and log the result to the console
+if (!isRaining) {
+  console.log("It's not raining.");
+} else {
+  console.log("It's raining.");
+}
 
 
+//Activity5:Ternary Operators
+
+let number=30
+let result=number>0?"Positive":"Negative"
+console.log(result);
